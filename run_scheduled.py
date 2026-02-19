30b0b00b23538db09767f12fc36148d0848a863b
JolaSonowo <84287612+JolaSonowo@users.noreply.github.com>
2026-02-19 21:29:27 +0100

    Delete run_scheduled.py

--- run_scheduled.py
+++
@@ -1,5 +0,0 @@
-from ngx_report import generate_files_for_today
-
-xlsx, docx, basename = generate_files_for_today()
-open(f"/var/reports/{basename}.xlsx", "wb").write(xlsx)
-open(f"/var/reports/{basename}.docx", "wb").write(docx)
